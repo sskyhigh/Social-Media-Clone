f8d044099d06addbed9f41324b9f3b409e433b14
sskyhigh <windowshao_123@yahoo.com>
2023-06-05 17:35:47 -0400

    added login functionality, patched small bugs

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,8 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 import Input from "@/app/components/inputs/input";
 
@@ -39,10 +41,23 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
-      // nextAuth Signin
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      }).then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials");
+        }
+        if (callback?.ok) {
+          toast.success("Success");
+        }
+      });
     }
   };
 
@@ -100,7 +115,7 @@ const AuthForm = () => {
 
           <div>
             <Button disabled={isLoading} fullwidth type="submit">
-              {variant === "LOGIN" ? "Sign in" : "Log In"}
+              {variant === "LOGIN" ? "Sign in" : "Register"}
             </Button>
           </div>
         </form>
